0bb076a477c3a1960797a60bf33ce70037d79b47
Tadashi Amada <38048071+Theara-Seng@users.noreply.github.com>
2023-04-25 11:49:43 +0700

    Update pxt.json, main.blocks, main.ts, joystic.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
-namespace screenMagics {
-    export function plotAts(index: number): void {
-        index |= 0
-        const x = Math.floor(index / 5);
-        const y = Math.floor(index % 5);
-        led.plot(x, y)
-    }
-}
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `);
