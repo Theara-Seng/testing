4a4effd19ee34cd89e3d09b4080334ab0b4cd0bb
Tadashi Amada <38048071+Theara-Seng@users.noreply.github.com>
2023-04-25 12:11:21 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `)
+WSJoyStick.onKEYs(KEYs.P, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # . . #
+        . # # # .
+        `)
+})
+WSJoyStick.onKey(KEY.C, function () {
+    WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Whole))
+})
+WSJoyStick.JoyStickInit()
