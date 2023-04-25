23d17f8bf3d2a7647c32efa820778c1974b74375
Tadashi Amada <38048071+Theara-Seng@users.noreply.github.com>
2023-04-25 11:37:15 +0700

    Update main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-basic.forever(function () {
-	
-})
+namespace screenMagics {
+    export function plotAts(index: number): void {
+        index |= 0
+        const x = Math.floor(index / 5);
+        const y = Math.floor(index % 5);
+        led.plot(x, y)
+    }
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,5 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+for (let i=0; i<25; ++i){
+    screenMagics.plotAts(i)
+    basic.pause(500)
+}
